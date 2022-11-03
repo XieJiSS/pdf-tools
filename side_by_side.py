3f2f3834e481481d4c983e70616e54cf854723bc
XieJiSS <XieJiSS@users.noreply.github.com>
2022-11-03 18:15:11 +0800

    feat: side-by-side merge pdf

---
+++ side_by_side.py
@@ -0,0 +1,61 @@
+import sys
+import os
+from PyPDF2 import PdfFileWriter, PdfFileReader
+from io import StringIO
+
+
+help_msg = r"""Usage:
+$ python3 side_by_side.py <input1.pdf> <input2.pdf>
+"""
+
+result_filename = "pdfs/side_by_side_merged.pdf"
+
+
+def merge_side_by_side(pdf_filename1, pdf_filename2):
+    """
+    Merge two pdf files side by side.
+    params:
+        pdf_filename1: the first pdf file
+        pdf_filename2: the second pdf file
+    """
+    input_pdf1 = PdfFileReader(open(pdf_filename1, "rb"), strict=False)
+    input_pdf2 = PdfFileReader(open(pdf_filename2, "rb"), strict=False)
+    output_pdf = PdfFileWriter()
+
+    w, h = input_pdf1.pages[0].mediabox.width, input_pdf1.pages[0].mediabox.height
+
+    # add an additional cover page to deal with Preview.app's two-page mode
+    output_pdf.add_blank_page(width=float(w), height=float(h))
+
+    for i in range(len(input_pdf1.pages)):
+        page1 = input_pdf1.pages[i]
+        page2 = input_pdf2.pages[i]
+        output_pdf.addPage(page1)
+        output_pdf.addPage(page2)
+
+    output_pdf.add_metadata(input_pdf1.metadata or input_pdf2.metadata or {})
+
+    with open(result_filename, "wb") as outputStream:
+        sys.stderr = StringIO()  # to suppress warnings
+        output_pdf.write(outputStream)
+        sys.stderr = sys.__stderr__
+
+    os.system(f"qpdf --linearize --replace-input \"{result_filename}\"")
+
+
+def main():
+    if len(sys.argv) < 3:
+        print(help_msg)
+        return
+
+    pdf_filename1 = sys.argv[1]
+    pdf_filename2 = sys.argv[2]
+    print(f"Merging {pdf_filename1} and {pdf_filename2}...")
+    merge_side_by_side(pdf_filename1, pdf_filename2)
+    print(f"Result PDF saved as {result_filename}")
+
+
+if __name__ == "__main__":
+    main()
+else:
+    print("This script is not intended to be imported.")
